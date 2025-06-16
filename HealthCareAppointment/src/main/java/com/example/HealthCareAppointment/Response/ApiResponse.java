627eb889f6278db23e33df3a06103bec86e5d1db
mailongvy <mlongvy0708@gmail.com>
2025-06-16 11:23:26 +0700

    update for controller

--- HealthCareAppointment/src/main/java/com/example/HealthCareAppointment/Response/ApiResponse.java
+++ HealthCareAppointment/src/main/java/com/example/HealthCareAppointment/Response/ApiResponse.java
@@ -1,9 +1,10 @@
 package com.example.HealthCareAppointment.Response;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-
+@AllArgsConstructor
 public class ApiResponse {
     private String message;
     private Object data;
