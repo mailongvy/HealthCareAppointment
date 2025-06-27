27b20699f5794b86a1febe272273e99a23d0fd34
mailongvy <mlongvy0708@gmail.com>
2025-06-27 08:57:51 +0700

    update, get by id, get by specialty for doctorcontroller

--- HealthCareAppointment/src/main/java/com/example/HealthCareAppointment/RestController/DoctorController.java
+++ HealthCareAppointment/src/main/java/com/example/HealthCareAppointment/RestController/DoctorController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,6 +40,44 @@ public class DoctorController {
                     .body(new ApiResponse(e.getMessage(), null));
         }
     }
+
+    @PutMapping("/doctor/update/{id}")
+    public ResponseEntity<ApiResponse> updateDoctor(@PathVariable Long id, @RequestBody Doctor doctor) {
+        try {
+            Doctor updated = doctorService.updateDoctor(doctor, id);
+            return ResponseEntity.ok(new ApiResponse("Update Doctor success", updated));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                                 .body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+    
+    @GetMapping("/doctor/{id}")
+    public ResponseEntity<ApiResponse> getDoctorById(@PathVariable Long id) {
+        try {
+            Doctor doctor = doctorService.getDoctorById(id);
+            return ResponseEntity.ok(new ApiResponse("Found", doctor));
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                                    .body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @GetMapping("/doctor/specialty")
+    public ResponseEntity<ApiResponse> getDoctorById(@PathVariable Long id) {
+        try {
+            Doctor doctor = doctorService.getDoctorById(id);
+            return ResponseEntity.ok(new ApiResponse("Found", doctor));
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                                    .body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+
+
 
     
 }
