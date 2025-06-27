96a6baa058b5c74603f835d7c7bfc6b83a139e21
mailongvy <mlongvy0708@gmail.com>
2025-06-27 09:33:37 +0700

    add delete method for doctor controller

--- HealthCareAppointment/src/main/java/com/example/HealthCareAppointment/RestController/DoctorController.java
+++ HealthCareAppointment/src/main/java/com/example/HealthCareAppointment/RestController/DoctorController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -76,6 +77,18 @@ public class DoctorController {
         }
     }
 
+    @DeleteMapping("/doctor/delete/{id}")
+    public ResponseEntity<ApiResponse> deleteDoctor(@PathVariable Long id) {
+        try {
+            doctorService.deleteDoctor(id);
+            return ResponseEntity.ok(new ApiResponse("Delete Doctor success", null));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                                 .body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    
 
 
 
