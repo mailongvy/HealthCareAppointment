3702a5b79e5ee3f1f8b7c4ef96508b6607da2aa0
mailongvy <mlongvy0708@gmail.com>
2025-06-30 09:18:16 +0700

    add deleteScheduleById method

--- HealthCareAppointment/src/main/java/com/example/HealthCareAppointment/Service/Schedule/IScheduleService.java
+++ HealthCareAppointment/src/main/java/com/example/HealthCareAppointment/Service/Schedule/IScheduleService.java
@@ -5,9 +5,11 @@ import java.util.List;
 import com.example.HealthCareAppointment.Model.Schedule;
 import com.example.HealthCareAppointment.Request.ScheduleRequest;
 
+
 public interface IScheduleService {
     public Schedule addSchedule(ScheduleRequest scheduleRequest);
     public Schedule getScheduleById(Long id);
     public List<Schedule> getAllSchedule();
+    public void deleteScheduleById(Long id);
 
 }
