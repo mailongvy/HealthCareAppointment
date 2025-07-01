d27d9bf3a2bea61c214e985ffd1d15ac3847d6ba
mailongvy <mlongvy0708@gmail.com>
2025-07-01 09:17:32 +0700

    update: add appointment for patient

--- HealthCareAppointment/src/main/java/com/example/HealthCareAppointment/Model/Patient.java
+++ HealthCareAppointment/src/main/java/com/example/HealthCareAppointment/Model/Patient.java
@@ -1,11 +1,14 @@
 package com.example.HealthCareAppointment.Model;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -32,6 +35,9 @@ public class Patient {
     private String gender;
 
     private String address;
+
+    @OneToMany(cascade=CascadeType.ALL, mappedBy="patient")
+    private List<Appointment> appointments;
 
     
 
