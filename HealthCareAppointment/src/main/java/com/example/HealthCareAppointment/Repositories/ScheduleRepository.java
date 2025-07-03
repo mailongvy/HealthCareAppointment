4e43da114962d44792d338cde51869212e0858a5
mailongvy <mlongvy0708@gmail.com>
2025-07-03 11:27:04 +0700

    add findByDoctorIdAndDayOfWeek method in schedulerepo

--- HealthCareAppointment/src/main/java/com/example/HealthCareAppointment/Repositories/ScheduleRepository.java
+++ HealthCareAppointment/src/main/java/com/example/HealthCareAppointment/Repositories/ScheduleRepository.java
@@ -1,5 +1,8 @@
 package com.example.HealthCareAppointment.Repositories;
 
+import java.time.DayOfWeek;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,5 @@ import com.example.HealthCareAppointment.Model.Schedule;
 
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-    
+    public List<Schedule> findByDoctorIdAndDayOfWeek(Long doctorId, DayOfWeek dayOfWeek);
 }
