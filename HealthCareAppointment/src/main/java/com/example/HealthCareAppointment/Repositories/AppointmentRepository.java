f14562087a2da76c48579a4534480f9bd9984e2e
mailongvy <mlongvy0708@gmail.com>
2025-07-03 08:54:03 +0700

    update appointmentRepository: extend JpaRepo

--- HealthCareAppointment/src/main/java/com/example/HealthCareAppointment/Repositories/AppointmentRepository.java
+++ HealthCareAppointment/src/main/java/com/example/HealthCareAppointment/Repositories/AppointmentRepository.java
@@ -1,5 +1,12 @@
 package com.example.HealthCareAppointment.Repositories;
 
-public interface AppointmentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.HealthCareAppointment.Model.Appointment;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+
 
 }
