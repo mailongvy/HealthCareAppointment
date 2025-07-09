92377d700008ab30895e142596dc80fec1ca798a
mailongvy <mlongvy0708@gmail.com>
2025-07-09 21:34:32 +0700

    add anotation for fields

--- HealthCareAppointment/src/main/java/com/example/HealthCareAppointment/Model/Notification.java
+++ HealthCareAppointment/src/main/java/com/example/HealthCareAppointment/Model/Notification.java
@@ -5,7 +5,16 @@ import java.time.LocalDateTime;
 import com.example.HealthCareAppointment.Enum.NotificationStatus;
 import com.example.HealthCareAppointment.Enum.NotificationType;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,18 +26,29 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Notification {
-
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable=false)
     private NotificationType type;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable=false)
     private NotificationStatus status;
 
     private String content;
 
     private LocalDateTime sendAt;
 
-    private Long recipantId;
+    @ManyToOne(cascade={CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name="patient_id", referencedColumnName="id")
+    private Patient patient;
+
+    @ManyToOne(cascade={CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name="doctor_id", referencedColumnName="id")
+    private Doctor doctor;
 
 
 
