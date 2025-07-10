df52fb6dd6da4aff8e298ff4472282714ed4d623
mailongvy <mlongvy0708@gmail.com>
2025-07-10 09:38:03 +0700

    create NotificationRepo

---
+++ HealthCareAppointment/src/main/java/com/example/HealthCareAppointment/Repositories/NotificationRepository.java
@@ -0,0 +1,11 @@
+package com.example.HealthCareAppointment.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.HealthCareAppointment.Model.Notification;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long>{
+
+}
