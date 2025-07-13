4442732401e40bad856d8c3cfb1ac861ea008878
mailongvy <mlongvy0708@gmail.com>
2025-07-13 11:51:34 +0700

    update notification 5 methods

--- HealthCareAppointment/src/main/java/com/example/HealthCareAppointment/Repositories/NotificationRepository.java
+++ HealthCareAppointment/src/main/java/com/example/HealthCareAppointment/Repositories/NotificationRepository.java
@@ -1,11 +1,19 @@
 package com.example.HealthCareAppointment.Repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.HealthCareAppointment.Enum.NotificationStatus;
+import com.example.HealthCareAppointment.Enum.NotificationType;
 import com.example.HealthCareAppointment.Model.Notification;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long>{
-
+    List<Notification> findByDoctorId(Long doctorId);
+    List<Notification> findByPatientId(Long patientId);
+    List<Notification> findByDoctorIdAndType(Long doctorId, NotificationType type);
+    List<Notification> findByPatientIdAndType(Long patientId, NotificationType type);
+    List<Notification> findByStatus(NotificationStatus status);
 }   
