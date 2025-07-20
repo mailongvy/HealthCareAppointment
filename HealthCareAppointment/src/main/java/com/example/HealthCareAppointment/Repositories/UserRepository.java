b8c4b9a070d388202dc68a76be4c0008c02a56e4
mailongvy <mlongvy0708@gmail.com>
2025-07-20 10:09:55 +0700

    adding existsByEmail

--- HealthCareAppointment/src/main/java/com/example/HealthCareAppointment/Repositories/UserRepository.java
+++ HealthCareAppointment/src/main/java/com/example/HealthCareAppointment/Repositories/UserRepository.java
@@ -6,4 +6,5 @@ import com.example.HealthCareAppointment.Model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     public User findByEmail(String email);
+    public boolean existsByEmail(String email);
 }
