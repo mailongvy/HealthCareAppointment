c440b1b1e61f7f70396f61c99e688f19b08fcbc0
mailongvy <mlongvy0708@gmail.com>
2025-07-20 10:19:20 +0700

    create  UserService

---
+++ HealthCareAppointment/src/main/java/com/example/HealthCareAppointment/Service/User/UserService.java
@@ -0,0 +1,58 @@
+package com.example.HealthCareAppointment.Service.User;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import com.example.HealthCareAppointment.Exception.ResourceNotFoundException;
+import com.example.HealthCareAppointment.Model.User;
+import com.example.HealthCareAppointment.Repositories.UserRepository;
+import com.example.HealthCareAppointment.Request.LoginRequest;
+import com.example.HealthCareAppointment.Security.Service.JWTService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements IUserService{
+    @Autowired
+    private final UserRepository userRepository;
+
+    private final AuthenticationManager authenticationManager;
+
+    private final JWTService jwtService;
+    
+    @Override
+    public User getUserById(Long userId) {
+        return userRepository.findById(userId)
+                            .orElseThrow(() -> new ResourceNotFoundException("User Not Found"));
+    }
+
+
+    @Override
+    public String verify(LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+        
+        // lưu thông tin xác thực vào spring context
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        // tạo token cho người dùng
+        String jwt = jwtService.generateToken(authentication);
+
+       
+
+        // trả về token đã tạo cho user
+        return jwt;
+    }
+
+    @Override
+    public User getAuthenticationUser() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAuthenticationUser'");
+    }
+
+}
